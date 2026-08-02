0ae68ba690be6ead5679cc29daa60b5acb9e9847
Alessandro2305 <alehsandromancano2016@gmail.com>
2026-08-02 08:12:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Registrador do App para instalação PWA
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Mantém requisições normais da rede
+});
